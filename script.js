a724ea06a0c756d5c232a05b6216193876a85d2a
berryl122 <balianmartin4@gmail.com>
2026-05-03 12:02:49 +0300

    майже все додав

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let cart = []; // Масив товарів у кошику
 async function fetchBooks() {
     const response = await fetch('books.json');
     const data = await response.json();
- books = data; // Оновлюємо глобальний масив для роботи addToCart
+    books = data; // Оновлюємо глобальний масив для роботи addToCart
     displayProducts(data);
+
 }
-function createBookCard (book) {
-return `<div class="col">
+function createBookCard(book) {
+    return `<div class="col">
         <div class="card h-100">
           <img src="${book.cover}" class="card-img-top" alt="...">
           <div class="card-body">
@@ -22,7 +23,6 @@ return `<div class="col">
         </div>
       </div>`
 }
-
 function displayProducts(products) {
     productsBlock.innerHTML = ''; // Очищаємо блок товарів
     books.forEach(book => {
@@ -30,6 +30,7 @@ function displayProducts(products) {
         productsBlock.innerHTML += card;
     });
 }
+
 function getJsonCookie(cookieName) {
     const allCookies = document.cookie.split('; ');
     const targetCookie = allCookies.find(row => row.startsWith(cookieName +
@@ -49,21 +50,56 @@ path=/`;
 }
 
 function addToCart(bookId) {
-    let book = books.find(b=>b.id == bookId);
+    let book = books.find(b => b.id == bookId);
     if (!book) return;
 
-    let item = cart.find(i=>i.id == bookId);
+    let item = cart.find(i => i.id == bookId);
     if (item) {
-         item.quantity+=1;
-    }else {
-        cart.push({...book, quantity: 1});
+        item.quantity += 1;
+    } else {
+        cart.push({ ...book, quantity: 1 });
     }
     console.log(cart);
-    saveJsonCookie('cart', cart, 3600*24);
+    saveJsonCookie('cart', cart, 3600 * 24);
+    displayCart();
 
 }
+function displayCart() {
+    cartContainer.innerHTML = '';
 
-document.addEventListener('DOMContentLoaded', () =>{
-    const productsBlock = document.querySelector ('#productsBlock');
+    if (cart.length === 0) {
+        cartContainer.innerHTML = '<p>Ваш кошик порожній</p>';
+       
+        return;
+    }
+
+    cart.forEach((book) => {
+        cartContainer.innerHTML += `
+        <div class="card border-0 border-bottom rounded-0">
+        <div class="card-body d-flex align-items-center gap-3 p-3">
+            <img src="img/${book.cover}" height="80">
+                <div class="flex-grow-1">
+                    <h5 class="card-title mb-1">${book.title}</h5>
+                    <p class="card-text text-muted mb-1">Кількість:${book.quantity}</p>
+                    <p class="card-text text-primary fw-bold mb-0">Ціна:${book.price} грн</p>
+                </div>
+            </div>
+        </div>
+        `;
+    });
+}
+function loadCart() {
+    const savedCart = getJsonCookie('cart');
+    if (savedCart !== null) {
+        cart = savedCart;
+        displayCart();
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const productsBlock = document.querySelector('#productsBlock');
     fetchBooks();
+    const cartContainer = document.querySelector('#cartItems');
 })
